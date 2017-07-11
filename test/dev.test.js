401b1e8b3c6ee48613ad3daab74c69f30566ed3e
Carlos Hoffmann <choffmann@ned.cl>
2017-07-11 16:20:29 -0400

    to commit something

--- test/dev.test.js
+++ test/dev.test.js
@@ -16,6 +16,17 @@ describe("Test suite DEMO", function () {
             })
     });
 
+    it("checking root method again just because I want", function (done) {
+        request
+            .get("/")
+        	.set("a", "b")
+            .expect({ "name": "CHE" })
+            .end(function (err, res) {
+
+                done();
+
+            })
+    });
 
 
 

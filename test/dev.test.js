b949df7adc33854d5a30cfc900894ebe02c829bc
Carlos Hoffmann <choffmann@ned.cl>
2017-07-14 17:17:35 -0400

    to check auth on jenkins

--- test/dev.test.js
+++ test/dev.test.js
@@ -43,6 +43,32 @@ describe("Test suite DEMO", function () {
     });
 
 
+    it("checking root method again just because I want", function (done) {
+        request
+            .get("/")
+            .set("a", "b")
+            .expect({ "name": "CHE" })
+            .end(function (err, res) {
+
+                done();
+
+            })
+    });
+
+    
+    it("checking root method again just because I want", function (done) {
+        request
+            .get("/")
+            .set("a", "b")
+            .expect({ "name": "CHE" })
+            .end(function (err, res) {
+
+                done();
+
+            })
+    });
+
+
 
 
 });
